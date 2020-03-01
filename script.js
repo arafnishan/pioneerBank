b2ca261be9caffe5a5c3ecd4870b8e206e5fe3b0
arafnishan <mahmud.nishan@gmail.com>
2020-03-02 01:31:30 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginButton=  document.getElementById('login');
+loginButton.addEventListener("click", function () {
+    const loginArea= document.getElementById('login-area');
+    loginArea.style.display='none';
+});
