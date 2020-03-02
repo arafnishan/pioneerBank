94bdb1b6d322fff0f8ac51f48466b6adcf097a9d
Araf Nishan <nishan@mazegeek.com>
2020-03-02 21:57:34 +0600

    Update two fields at the same time, create function done

--- script.js
+++
@@ -1,5 +0,0 @@
-const loginButton=  document.getElementById('login');
-loginButton.addEventListener("click", function () {
-    const loginArea= document.getElementById('login-area');
-    loginArea.style.display='none';
-});
